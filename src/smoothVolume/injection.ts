65a0cd1f89aa9af5df51f192b2513ecf24077e97
EhsanFox <EhsanFox@users.noreply.github.com>
2022-04-20 23:41:52 +0430

    trying to use ts-expect-error

--- src/smoothVolume/injection.ts
+++ src/smoothVolume/injection.ts
@@ -2,10 +2,9 @@ import { VolumeTransformer as VolumeTransformerMock } from "./VolumeTransformer"
 
 export function injectSmoothVolume() {
     try {
-        /* eslint-disable */
-        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
         const mod = require("prism-media") as typeof import("prism-media") & { VolumeTransformer: typeof VolumeTransformerMock };
-        /* eslint-enable */
 
         if (typeof mod.VolumeTransformer.hasSmoothing !== "boolean") {
             Reflect.set(mod, "VolumeTransformer", VolumeTransformerMock);
