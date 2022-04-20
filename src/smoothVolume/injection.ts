a28790f23d2effe0346c7832f8ddb0831f42d1b7
EhsanFox <EhsanFox@users.noreply.github.com>
2022-04-20 23:37:53 +0430

    fix: Disabling eslint for ignoring tsc errors

--- src/smoothVolume/injection.ts
+++ src/smoothVolume/injection.ts
@@ -2,8 +2,10 @@ import { VolumeTransformer as VolumeTransformerMock } from "./VolumeTransformer"
 
 export function injectSmoothVolume() {
     try {
-        // eslint-disable-next-line
+        /* eslint-disable */
+        // @ts-ignore
         const mod = require("prism-media") as typeof import("prism-media") & { VolumeTransformer: typeof VolumeTransformerMock };
+        /* eslint-enable */
 
         if (typeof mod.VolumeTransformer.hasSmoothing !== "boolean") {
             Reflect.set(mod, "VolumeTransformer", VolumeTransformerMock);
